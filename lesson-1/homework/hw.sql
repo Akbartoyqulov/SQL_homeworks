4c264d3e7f629f33b5570d8902ec4128a68a10fb
Akbartoyqulov <pesuchun426@gmail.com>
2025-09-27 12:46:25 +0500

    Create hw.sql

---
+++ lesson-1/homework/hw.sql
@@ -0,0 +1,11 @@
+EASY tasks
+1
+data - ahvkbaifb
+database - sadbvabv
+
+
+2
+High Availability and Disaster Recovery= bu server uzluksz iwlaydigan va malumotlarni himoya qiladgan server
+va yana qoshimcha qilsak u osha birorta malumot ochb ketsa yoki nimadur bolsa malumotlarni on mavjudliylik guruhida klasterkaydi va bazalarda aks ettiradi
+Advanced Security Features:
+	
