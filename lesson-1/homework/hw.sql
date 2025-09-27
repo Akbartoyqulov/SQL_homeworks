ad678e5ed176a5dcec30cd52c9d9c94d0218e62b
Akbartoyqulov <pesuchun426@gmail.com>
2025-09-27 12:47:13 +0500

    Update hw.sql

--- lesson-1/homework/hw.sql
+++ lesson-1/homework/hw.sql
@@ -9,3 +9,4 @@ High Availability and Disaster Recovery= bu server uzluksz iwlaydigan va malumot
 va yana qoshimcha qilsak u osha birorta malumot ochb ketsa yoki nimadur bolsa malumotlarni on mavjudliylik guruhida klasterkaydi va bazalarda aks ettiradi
 Advanced Security Features:
 	
+3 davomi
